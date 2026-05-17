dbf044fe3267f3842fdd476d309d3feb0c46e0bc
Ankana talukdar <150015591+AnkanaTalukdar@users.noreply.github.com>
2026-05-17 17:21:52 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Environment</key>
+    <string>Production</string>
+    <key>RememberMe</key>
+    <true/>
+    <key>BackendAPIKey</key>
+    <string>AIzaSyA1B2C3D4E5F6G7H8I9J0K1L2M3N4O5P6</string>
+</dict>
+</plist>
